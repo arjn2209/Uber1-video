2e678e22c964c6ca5b435079b32502f00ff23596
Arjun Garg <arjungarg360@gmail.com>
2025-07-09 10:48:30 +0530

    user authenthication done

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const userModel = require('../models/user.models');
+const jwt = require('jsonwebtoken');    
+const bcrypt = require('bcrypt');
+const blacklistTokenModel = require('../models/blacklistToken.model');
+
+module.exports.authUser=async(req, res, next) => {
+    const token=req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+
+    if(!token) {
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+
+    const isBlacklisted = await BlacklistTokenModel.findOne({token: token});
+
+    if(isBlacklisted) {
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded.id);
+        req.user = user;
+        return next();
+    }catch (error) {
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+}
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const {body}=require("express-validator")
 const userController=require('../controllers/user.controller');
+const authMiddleware=require('../middlewares/auth.middleware');
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid Email'),
@@ -13,5 +14,12 @@ router.post('/login',[
     body('email').isEmail().withMessage('Invalid Email'),
     body('password').isLength({min:6}).withMessage('Password must be at least 6 characters long')
 ],userController.loginUser);
+
+router.get('/profile', authMiddleware.authUser ,userController.getUserProfile);
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logged out successfully' });
+});
 
 module.exports=router;
